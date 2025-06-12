15a65a04d7acc633cb91a60e6e08dc7bd67d2ca6
camilamoyac <cami.csmc@gmail.com>
2025-06-12 00:30:31 -0400

    replaced ProductData for ExternalServices

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 import Alert from "./alert";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, listElement);
 
